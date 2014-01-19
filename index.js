6a54c6283df75843fd7f024669817324d74a1c20
Joe Hudson <joehud@gmail.com>
2014-01-19 16:28:48 -0500

    add true server and builder plugins

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var builder = require('./lib/builder'),
     gulp = require('gulp'),
     clean = require('gulp-clean'),
     through = require("through2"),
-    server = require("./lib/server");
+    path = require('path'),
+    lifecycleEvents = ['onSectionPreBrowserify', 'onSectionPostBrowserify', 'onSectionPreDest', 'onPublicPreDest'],
+    plugins = [],
+    devServerPlugins = [];
 
 function checkOptions(options, mergeOptions) {
     options = options || {};
@@ -19,11 +22,7 @@ function checkOptions(options, mergeOptions) {
         port: 8080,
         proxy: function(gulpOptions, requestOptions, callback) {
             callback();
-        },
-        onSectionPreBrowserify: nop,
-        onSectionPostBrowserify: nop,
-        onSectionPreDest: nop,
-        onPublicResource: nop
+        }
     }
     for (var i in _defaults) {
         if (options[i] === undefined) {
@@ -47,12 +46,74 @@ function checkOptions(options, mergeOptions) {
     return options;
 }
 
+function normalizePlugins(plugins) {
+    var _plugins = {}
+    for (var i in plugins) {
+        var plugin = plugins[i];
+        for (var j in lifecycleEvents) {
+            var eventName = lifecycleEvents[j],
+                lifecyclePlugin = plugin[eventName];
+            if (lifecyclePlugin) {
+                // a plugin exists for the current event
+                var pluginPipeline = _plugins[eventName];
+                if (!pluginPipeline) {
+                    // initialize
+                    pluginPipeline = lifecyclePlugin();
+                } else {
+                    // hook it into the pipeline
+                    pluginPipeline = pluginPipeline.pipe(lifecyclePlugin());
+                }
+            }
+        }
+    }
+    return _plugins;
+}
+
+function initPlugins(options) {
+    var _plugins = options.plugins || [];
+
+    // add direct lifecicye events from options as the first plugin
+    var rootPlugin = {};
+    for (var i in lifecycleEvents) {
+        var eventName = lifecycleEvents[i];
+        rootPlugin[eventName] = options[eventName];
+    }
+    plugins.push(rootPlugin);
+
+    // add all registered plugins
+    for (var i in _plugins) {
+        var plugin = _plugins[i];
+        plugins.push(plugin);
+        if (plugin.devServer) {
+            devServerPlugins.push(plugin.devServer);
+        }
+    }
+
+    // normalize the plugins into a single piped object
+    options.normalizedPlugins = normalizePlugins(plugins);
+
+
+    // add the root devserver plugin
+    devServerPlugins.push(options.devServer || {
+        onRequest: function(requestOptions, pluginOptions, callback) {
+            var uri = requestOptions.uri;
+            if (uri === '/') {
+                uri = options.defaultServResource;
+            }
+            callback( {
+                fileName: path.join(requestOptions.base, uri)
+            })
+        }
+    });
+}
+
 function _clean(src) {
     gulp.src(src, {read: false}).pipe(clean());
 }
 
 module.exports = function(options) {
     options = checkOptions(options);
+    initPlugins(options);
 
     return {
         injectTasks: function(gulp, tasks) {
@@ -85,7 +146,7 @@ module.exports = function(options) {
 
         watchrun: function() {
             builder(checkOptions(options, checkOptions(options, {buildType: gulp.env.type || 'dev', watch: true})));
-            server.start(options);
+            require("./lib/server")(checkOptions(options, { plugins: devServerPlugins })).start();
         },
 
         clean: function() {
